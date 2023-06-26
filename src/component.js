417bbff1df94b0bd9509dc384d65d8462c48bacb
adhxabre <abeldustin06@gmail.com>
2023-06-26 11:12:17 +0700

    Component

--- src/component.js
+++ src/component.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 // import component Header
+import Header from "./components/header";
 
 function Component() {
   return (
-    //   Code Here
-    <div></div>
+    <div>
+      <Header />
+      <Content />
+    </div>
   );
 }
 
 // Create a new component here
+function Content() {
+  return (
+    //   Code Here
+    <div>
+      <h1>This is a content</h1>
+      <button>Click me</button>
+    </div>
+  );
+}
 
 export default Component;
