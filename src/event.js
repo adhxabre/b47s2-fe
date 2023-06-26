f72cedb57669585274e3c8e7db06c430b81192bb
adhxabre <abeldustin06@gmail.com>
2023-06-26 11:39:37 +0700

    Event

--- src/event.js
+++ src/event.js
@@ -2,10 +2,25 @@ import React from "react";
 
 function Event() {
   //Create function here
+  function Greetings() {
+    return alert("Guten Morgen!");
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>If you press here, it will alert you using alert function</p>
+      <button
+        onClick={() => {
+          alert("Selamat Pagi!");
+        }}
+      >
+        Click Me
+      </button>
+
+      <p>If you press here, it will alert you using alert function</p>
+      <button onClick={Greetings}>Click Me</button>
+    </div>
   );
 }
 
