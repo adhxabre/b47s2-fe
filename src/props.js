b32b44e91ef6abad4ebf3d56f949f3ef892bced4
adhxabre <abeldustin06@gmail.com>
2023-06-26 13:05:28 +0700

    Props

--- src/props.js
+++ src/props.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 //Import Components
+import List from "./components/list";
 
 function Props() {
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>On the image using default page, namely src</p>
+      <img
+        src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1542354895/ulaqus4ev5ihhqkpbhuz.jpg"
+        alt=""
+      />
+
+      <List Data="Bebek Merah Warnanya (BMW)" />
+      <List Data="Mercedez" />
+      <List Data="Lemberjember" />
+    </div>
   );
 }
 
