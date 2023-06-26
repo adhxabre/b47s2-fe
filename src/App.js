2ab81a41cf9a05e4514c74fc5c921d82fe377120
adhxabre <abeldustin06@gmail.com>
2023-06-26 15:31:53 +0700

    Nav & Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import necessary object from react-router-dom
+import { Routes, Route, Link } from "react-router-dom";
 
 // import "pages" component here
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/SignIn";
 
 function App() {
   return (
@@ -10,8 +15,30 @@ function App() {
     <>
       <div>
         {/* Setup navigation here */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* code <Routes> and <Route> below, according to path and component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
       </div>
-      {/* code <Routes> and <Route> below, according to path and component */}
     </>
   );
 }
