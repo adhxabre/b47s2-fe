43c0bae4a96e2b2fa5f5910f0e660feaf3cf1886
adhxabre <abeldustin06@gmail.com>
2023-06-26 13:26:17 +0700

    State

--- src/state.js
+++ src/state.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function State() {
   //init State
+  const [counter, setCounter] = useState(0);
 
   //Create Function Add
+  function Add() {
+    return setCounter(counter + 1);
+  }
 
   //Create Function Less
+  function Less() {
+    return setCounter(counter - 1);
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        If you click Add button, it will increase by one. But if you click Less
+        button, it will decrease by one
+      </p>
+
+      <h1>{counter}</h1>
+
+      <button onClick={Add}>Add</button>
+      <button onClick={Less}>Less</button>
+    </div>
   );
 }
 
