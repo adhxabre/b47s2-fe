48f22ebefd54290fc0a098c7a6767a28b9017104
adhxabre <abeldustin06@gmail.com>
2023-06-26 11:27:36 +0700

    Embed Expressions

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,12 +2,19 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function getMajor() {
+    return "Fullstack Developer";
+  }
 
   //Create a variable here
+  let companyName = "DumbWays.ID";
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      Welcome to {companyName} class {/* Then how about using function? */}
+      {getMajor()}
+    </div>
   );
 }
 
