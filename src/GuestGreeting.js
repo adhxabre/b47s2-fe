3931c25e8243c8fca046de8fef0292046f47af1c
adhxabre <abeldustin06@gmail.com>
2023-06-27 11:49:01 +0700

    lifecycle func

--- src/GuestGreeting.js
+++ src/GuestGreeting.js
@@ -1,13 +1,17 @@
 //import useEffect here
+import { useEffect } from "react";
 
 export default function GuestGreeting() {
+  // Create DidMount & WillUnmount with useEffect
+  // inside it can print "Guest Greeting Component Did Mount" & "Guest Greeting Component Will Unmount"
+  useEffect(() => {
+    alert("Greetings Bro!");
+    return alert("Sampai jumpa bro!");
+  });
 
-    // Create DidMount & WillUnmount with useEffect 
-    // inside it can print "Guest Greeting Component Did Mount" & "Guest Greeting Component Will Unmount"
-
-    return (
-        <div className="text-center h1 bg-secondary text-light py-5">
-            Please Login !
-        </div>
-    )
+  return (
+    <div className="text-center h1 bg-secondary text-light py-5">
+      Please Login !
+    </div>
+  );
 }
